0ceb092d5e9583e0a676ca1a8c619d6df272b869
godotdotdot <chukui0202@gmail.com>
2021-10-16 01:00:52 +0800

    feat: update

--- components/partners/index.tsx
+++ components/partners/index.tsx
@@ -54,8 +54,173 @@ import blockvita from '../../public/event-support/new/blockvita.png';
 import dg from '../../public/event-support/new/dg.png';
 import cryptomondays from '../../public/event-support/new/cryptomondays.jpeg';
 import tianmo from '../../public/event-support/new/tianmo.png';
+import metacat from '../../public/partners-logo/new/metacat.png';
+import hotneutrostudio from '../../public/media/hot-neutro-studio.png';
+import artgee from '../../public/media-support/artgee.png';
+import cesme from '../../public/event-support/new/cesm-E.png';
+import mrameng from '../../public/media/mrmeng.png';
+import chainnews from '../../public/media-support/media15.png';
+import cointelegraphcn from '../../public/media-support/cointelegraphcn.png';
+import panews from '../../public/media-support/media18.png';
+import chaindd from '../../public/media-support/media12.png';
+import blockbeats from '../../public/media-support/media16.png';
+import odaily from '../../public/media-support/media20.png';
+import tuoluocaijing from '../../public/media-support/media4.png';
+import tangent from '../../public/media-support/media8.png';
+import orangepaper from '../../public/media-support/media10.png';
+import tencent from '../../public/media-support/media19.png';
+import jazzyear from '../../public/media-support/media6.png';
+import tmtpost from '../../public/media-support/media21.png';
+import geekpark from '../../public/media-support/geekpark.png';
+import jiemiannews from '../../public/media-support/jiemiannews.png';
 
 import styles from './index.module.less';
+
+const LIST = [
+  {
+    title: 'Legendary Partners',
+    list: [
+      { avatar: sponsors9, link: '', name: 'Ownership Labs' },
+      { avatar: sponsors8, link: '', name: 'Litentry' },
+      { avatar: sponsors13, link: '', name: 'UTU.ONE' },
+    ],
+  },
+  {
+    title: 'Epic Partners',
+    list: [
+      { avatar: sponsors1, link: '', name: 'Polygon' },
+      { avatar: sponsors14, link: '', name: 'FLOW' },
+      { avatar: sponsors, link: '', name: 'Harmony' },
+      { avatar: sponsors2, link: '', name: 'Nash Metaverse' },
+      { avatar: sponsors10, link: '', name: 'RMRK' },
+      { avatar: sponsors4, link: '', name: 'MixMarvel' },
+      { avatar: sponsors6, link: '', name: 'Tezos' },
+      { avatar: avatar6, link: '', name: 'Mynft' },
+      { avatar: sponsors12, link: '', name: 'Social Future' },
+      { avatar: sponsors3, link: '', name: 'Follow.top' },
+    ],
+  },
+  {
+    title: 'Top Partners',
+    list: [
+      { avatar: avatar3, link: '', name: 'MOCA (Museum of Crypto Art)' },
+      { avatar: sponsors7, link: '', name: 'My Crypto Profile' },
+      { avatar: avatar5, link: '', name: 'Mask Network' },
+    ],
+  },
+  {
+    title: 'Industry Partners',
+    list: [
+      { avatar: support, link: '', name: 'Binance' },
+      { avatar: support9, link: '', name: 'imToken' },
+      { avatar: support10, link: '', name: 'Yuanyuzhou Venture' },
+      { avatar: support16, link: '', name: 'Digital Renaissance' },
+    ],
+  },
+  {
+    title: 'Exhibition Support',
+    list: [
+      { avatar, link: '', name: 'Technology Art' },
+      { avatar: avatar9, link: '', name: 'Cryptoart.ai' },
+      { avatar: bonnybb, link: '', name: 'Bonnybb.eth' },
+      { avatar: metacat, link: '', name: 'MetaCat' },
+      { avatar: support17, link: '', name: 'Super ACG' },
+      { avatar: support3, link: '', name: 'Innovation+ Art' },
+      { avatar: tianmo, link: '', name: 'Talent mart' },
+    ],
+  },
+  {
+    title: 'Content Support',
+    list: [
+      { avatar: support8, link: '', name: 'Crypto Playground' },
+      { avatar: avatar1, link: '', name: 'M360' },
+      { avatar: avatar11, link: '', name: 'Next Scene' },
+      { avatar: hotneutrostudio, link: '', name: 'Hot Neutron Studio' },
+    ],
+  },
+  {
+    title: 'Metaverse Support',
+    list: [
+      { avatar: dg, link: '', name: 'Decentral Games' },
+      { avatar: sponsors11, link: '', name: 'MetaV' },
+      { avatar: metaestate, link: '', name: 'MetaEstate' },
+      { avatar: support15, link: '', name: 'Sandbox' },
+      { avatar: support11, link: '', name: 'OVR' },
+      { avatar: support2, link: '', name: 'Evolution Land' },
+      { avatar: avatar10, link: '', name: 'Mirror World' },
+    ],
+  },
+  {
+    title: 'Art Support',
+    list: [
+      { avatar: support7, link: '', name: 'Hanshan Art Museum' },
+      { avatar: support12, link: '', name: 'Arium' },
+      { avatar: cesme, link: '', name: 'CESM' },
+      { avatar: artgee, link: '', name: 'ArtGee' },
+    ],
+  },
+  {
+    title: 'Music Support',
+    list: [
+      { avatar: omarblack, link: '', name: 'OMARBLACK' },
+      { avatar: support4, link: '', name: 'Caotai Music' },
+      { avatar: support14, link: '', name: 'Sound Blanc' },
+    ],
+  },
+  {
+    title: 'Community Support',
+    list: [
+      { avatar: blockvita, link: '', name: 'BlockVita' },
+      { avatar: avatar7, link: '', name: 'Blocklike' },
+      { avatar: support6, link: '', name: 'MetaVenturer' },
+      { avatar: support1, link: '', name: 'OG Labs' },
+      { avatar: support5, link: '', name: 'Sssnodes' },
+      { avatar: avatar8, link: '', name: 'Lying Gallery' },
+      { avatar: cryptomondays, link: '', name: 'Crypto Mondays' },
+      { avatar: mrameng, link: '', name: 'Mr Ameng' },
+    ],
+  },
+  {
+    title: 'Metaverse Exclusive Live Stream',
+    list: [{ avatar: support18, link: '', name: 'Decentraland' }],
+  },
+  {
+    title: 'Strategic Media Partners',
+    list: [
+      { avatar: chainnews, link: '', name: 'ChainNews' },
+      { avatar: cointelegraphcn, link: '', name: 'Cointelegraph CN' },
+      { avatar: panews, link: '', name: 'PANews' },
+    ],
+  },
+  {
+    title: 'Blockchain Media Partners',
+    list: [
+      { avatar: chaindd, link: '', name: 'CHAINDD' },
+      { avatar: blockbeats, link: '', name: 'BlockBeats' },
+      { avatar: odaily, link: '', name: 'ODAILY' },
+      { avatar: tuoluocaijing, link: '', name: 'tuoluocaijing' },
+      { avatar: tangent, link: '', name: 'TANGENT' },
+      { avatar: orangepaper, link: '', name: 'orangepaper' },
+    ],
+  },
+  {
+    title: 'Tech Media Partners',
+    list: [
+      { avatar: tencent, link: '', name: '腾讯科技（区块链鹅探长）' },
+      { avatar: jazzyear, link: '', name: 'JAZZYEAR' },
+      { avatar: jiemiannews, link: '', name: '界面新闻' },
+      { avatar: tmtpost, link: '', name: 'TMTPOST' },
+      { avatar: geekpark, link: '', name: 'GEEKPARK' },
+    ],
+  },
+  {
+    title: 'Event Coordinators',
+    list: [
+      { avatar: avatar2, link: '', name: 'NFT4Metaverse' },
+      { avatar: avatar4, link: '', name: 'CryptoC' },
+    ],
+  },
+];
 
 export default function partnersPartners() {
   const cls = classnames(styles.container);
@@ -63,79 +228,20 @@ export default function partnersPartners() {
     <div className="relative flex justify-center items-center w-screen h-screen bg-black">
       <PageTitle title="Partners" subTitle="合作方" />
       <div className={cls}>
-        <div className={styles.box}>
-          <h4 className={`${styles.title} text-left text-white fonts-kumar-one mt-40`}>
-            Event Owners
-          </h4>
-          <div className="grid grid-cols-2 lg:grid-cols-5">
-            <Card avatar={avatar9} link={''} name="Cryptoart.ai" />
-            <Card avatar={avatar4} link={''} name="CryptoC" />
-            <Card avatar={sponsors8} link={''} name="Litentry" />
-            <Card avatar={avatar5} link={''} name="Mask Network" />
-            <Card avatar={avatar3} link={''} name="MOCA (Museum of Crypto Art)" />
-            <Card avatar={sponsors7} link={''} name="My Crypto Profile" />
-            <Card avatar={avatar6} link={''} name="Mynft" />
-            <Card avatar={avatar11} link={''} name="NeXT SCENE" />
-            <Card avatar={avatar2} link={''} name="NFT4Metaverse" />
-            <Card avatar={avatar} link={''} name="Technology Art" />
-          </div>
-        </div>
-        <div className={styles.box}>
-          <h4 className={`${styles.title} text-left text-white fonts-kumar-one mt-40`}>
-            Top Partners
-          </h4>
-          <div className="grid grid-cols-2 lg:grid-cols-5">
-            <Card avatar={sponsors5} link={''} name="DaVinci Gallery" />
-            <Card avatar={sponsors14} link={''} name="Flow" />
-            <Card avatar={sponsors3} link={''} name="Follow.top" />
-            <Card avatar={sponsors} link={''} name="Harmony" />
-            <Card avatar={sponsors4} link={''} name="MixMarvel" />
-            <Card avatar={sponsors2} link={''} name="NASH Metaverse" />
-            <Card avatar={sponsors9} link={''} name="Ownership Labs" />
-            <Card avatar={sponsors1} link={''} name="Polygon" />
-            <Card avatar={sponsors10} link={''} name="RMRK" />
-            <Card avatar={sponsors12} link={''} name="Social Future" />
-            <Card avatar={sponsors6} link={''} name="Tezos" />
-            <Card avatar={sponsors13} link={''} name="utu.one" />
-          </div>
-        </div>
-        <div className={styles.box}>
-          <h4 className={`${styles.title} text-left text-white fonts-kumar-one mt-40`}>
-            Event Support
-          </h4>
-          <div className="grid grid-cols-2 lg:grid-cols-5">
-            <Card avatar={support12} link={''} name="Arium" />
-            <Card avatar={support} link={''} name="Binance" />
-            <Card avatar={avatar7} link={''} name="Blocklike" />
-            <Card avatar={blockvita} link={''} name="BlockVita" />
-            <Card avatar={bonnybb} link={''} name="Bonnybb.eth" />
-            <Card avatar={support4} link={''} name="Caotai Music" />
-            <Card avatar={cryptomondays} link={''} name="Crypto Mondays" />
-            <Card avatar={support8} link={''} name="Crypto Playground" />
-            <Card avatar={support18} link={''} name="Decentraland" />
-            <Card avatar={dg} link={''} name="Decentral Games" />
-            <Card avatar={support16} link={''} name="Digital Renaissance" />
-            <Card avatar={support2} link={''} name="Evolution Land" />
-            <Card avatar={support7} link={''} name="Hanshan Art Museum" />
-            <Card avatar={support9} link={''} name="imToken" />
-            <Card avatar={support3} link={''} name="Innovation+ Art" />
-            <Card avatar={avatar8} link={''} name="Lying Gallery" />
-            <Card avatar={avatar1} link={''} name="M 360" />
-            <Card avatar={metaestate} link={''} name="MetaEstate" />
-            <Card avatar={sponsors11} link={''} name="MetaV" />
-            <Card avatar={support6} link={''} name="MetaVenturer" />
-            <Card avatar={avatar10} link={''} name="Mirror World" />
-            <Card avatar={support1} link={''} name="OG Labs" />
-            <Card avatar={omarblack} link={''} name="OMARBLACK" />
-            <Card avatar={support11} link={''} name="OVR" />
-            <Card avatar={support15} link={''} name="Sandbox" />
-            <Card avatar={support14} link={''} name="Sound Blanc" />
-            <Card avatar={support5} link={''} name="Sssnodes" />
-            <Card avatar={support17} link={''} name="Super ACG" />
-            <Card avatar={tianmo} link={''} name="Talent mart" />
-            <Card avatar={support10} link={''} name="Yuanyuzhou Venture" />
-          </div>
-        </div>
+        {LIST.map((item) => {
+          return (
+            <div className={styles.box}>
+              <h4 className={`${styles.title} text-left text-white fonts-kumar-one mt-40`}>
+                {item.title}
+              </h4>
+              <div className="grid grid-cols-2 lg:grid-cols-5">
+                {item.list.map((partner) => {
+                  return <Card avatar={partner.avatar} link={partner.link} name={partner.name} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
         <div className={styles.text}>
           <div>*The above partners are in alphabetical order and are being added...</div>
         </div>
